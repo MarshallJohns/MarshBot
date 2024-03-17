abc599d8bba335e2f025961c1b265e972e00ab56
MarshallJohns <marshalljohnson193@gmail.com>
2024-03-17 11:52:44 -0600

    removed discord key

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 
+
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!',intents=intents)
 
@@ -12,9 +13,26 @@ async def on_ready():
 
 @client.command()
 async def hello(ctx):
-    print(ctx.author.global_name)
-    await ctx.send(f"Hello @{ctx.author.name}")
+    print(ctx.author)
+    await ctx.send(f"Hello <@{ctx.author.id}>")
+
+@client.command(pass_context = True)
+async def join(ctx):
+    if(ctx.author.voice):
+        channel  = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send(f"You're not in a voice channel <@{ctx.author.id}>, you fucking idiot.")
+@client.command(pass_context = True)
+async def leave(ctx):
+    if(ctx.voice_client):
+        channel  = ctx.message.author.voice.channel
+        await channel.guild.voice_client.disconnect()
+        await ctx.send("Bye Bye.")
+    else:
+        await ctx.send(f"I'm not in a voice channel <@{ctx.author.id}>, you fucking idiot.")
 
 
 
-client.run('MTIxNTc5MzQ4NDkxMzU3ODEyNg.GWKkPj.n-X32WjVT9YsjmOuL4jBnGGxebjhJx_NFtSTZg')
+
+client.run('')
